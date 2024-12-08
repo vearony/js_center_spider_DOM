99708ced7b98ab71471107103c684eb265146f2a
Veronika Vasyliuk <verka0218@gmail.com>
2024-12-08 18:14:08 +0200

    make build

---
+++ index.5dad1628.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");e.style.position="relative";var i=e.getBoundingClientRect(),l=t.getBoundingClientRect(),n=(i.height-l.height)/2,o=(i.width-l.width)/2;t.style.top=n+"px",t.style.left=o+"px";
+//# sourceMappingURL=index.5dad1628.js.map
